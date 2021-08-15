0ec52f3f85d154a293b07f93a689a6144850b961
Shrikant Yadav <shrikant9907@gmail.com>
2021-08-15 13:06:58 +0530

    logo

--- learncodes/logo.php
+++ learncodes/logo.php
@@ -1,31 +1,7 @@
-<?php 
-include_once 'header.php';
-?>
-
 <!DOCTYPE html>
 <html lang="en-US">
 
 <head>
-
-  <title>codematra.com</title>
-
-  <!-- Required meta tags -->
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-  <meta property="og:title" content="" />
-  <meta property="og:description" content="" />
-
-  <meta property="og:url" content="" />
-  <meta property="og:image" content="" />
-
-  <meta name="google-site-verification" content="" />
-  <meta name="robots" content="noindex, nofollow" />
-
-  <!-- <meta name="site-icon" content="images/favicon.png" />
-  <link rel="icon" type="image/x-icon" href="images/favicon.png"/>
-  <link rel="shortcut icon" href="images/favicon.png" /> -->
-
   <!-- required Fonts -->
   <link rel="preconnect" href="https://fonts.gstatic.com">
   <link href="https://fonts.googleapis.com/css2?family=Rajdhani:wght@300;500&display=swap" rel="stylesheet">
@@ -37,10 +13,7 @@ include_once 'header.php';
 
 
   <!-- required CSS -->
-  <link rel="stylesheet" href="../../../css/bootstrap.min.css" />
   <link rel="stylesheet" href="../../../css/all.min.css" />
-  <link rel="stylesheet" href="../../../css/slick.css" />
-  <link rel="stylesheet" href="../../../css/slick-theme.css" />
   <link rel="stylesheet" href="../../../css/components.css" />
   <link rel="stylesheet" href="../../../css/utils.css" />
   <link rel="stylesheet" href="../../../css/learning-lp.css" />
@@ -53,31 +26,30 @@ include_once 'header.php';
       display: flex;
       justify-content: center;
       align-items: center;
+      flex-direction: column;
       font-weight: 700;
+      text-decoration: unset;
+      color: #000000;
+      margin-top:100px;
     }
 
     .site_logo a .logo_text{
-      display: flex;
-    }
-
-    .icon {
       font-family: 'Rajdhani', sans-serif;
       font-size: 40px;
-      top: -3px;
-      position: relative;
-      margin-right: 5px;
+      letter-spacing: 1px;
+      color: #FB1D1D;
     }
 
-    .icon span:last-child {
-      /* left: -2px; */
-      position: relative;
+    .site_logo a .logo_text span{
+      color: #944DA0;
     }
 
     .site_logo .tag_line {
-      font-size: 15px;
+      font-size: 11px;
       font-weight: 400;
-      letter-spacing: 4.5px;
-      left: 4px;
+      letter-spacing: 0.7px;
+      line-height: 18px;
+      text-transform: uppercase;
   }
 
   </style>
@@ -89,33 +61,12 @@ include_once 'header.php';
   <div class="site_logo with_tagline">
     <a href="#">
       <div class="logo_text">
-        <div class="icon">{<span class="text-primary">G</span><span class="text-dark">W</span><span class="text_secondary">C</span>></div>
+        Code <span>Matra</span>
       </div> 
-      <span class="tag_line">A better place to learn coding.</span>
+      <div class="tag_line">Learn code from the basics</div>
     </a>
   </div>
-
  
-  <!-- jQuery -->
-  <script src="../../../js/jquery.min.js" type="text/javascript"></script> 
-  
-  <!-- Fonts -->
-  <script src="../../../js/all.min.js" type="text/javascript"></script>
-
-  <!-- Popper JS -->
-	<script src="../../../js/popper.min.js" type="text/javascript"></script>
-
-	<!-- Bootstrap -->
-	<script src="../../../js/bootstrap.min.js" type="text/javascript"></script>
-
-	<!-- Slick Slider -->
-  <script type="text/javascript" src="../../../js/slick.min.js"></script>
-  
-  <!-- Parallax Js -->
-	<script type="text/javascript" src="../../../js/parallax.min.js"></script>
-
-	<!-- Custom Script -->
-	<script type="text/javascript" src="js/custom.js"></script>
 
 </body>
 </html>  
